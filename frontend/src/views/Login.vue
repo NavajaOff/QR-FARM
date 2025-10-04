f29bdeebfa4023eb1da110dec5aec10b77940217
LAPTOP-JRUMML2J\ronal <ronaldbejarano147>
2025-10-04 07:12:06 -0500

    arreglo login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -25,10 +25,7 @@
                     <div class="col-12 col-md-6">
                         <div class="p-3 p-sm-4 p-lg-5 h-100 d-flex flex-column justify-content-center bg-white">
                             <div class="text-center mb-4">
-                                <img src="{{ url_for('static', filename='images/vacas-grupo.jpg') }}" 
-                                    alt="Grupo de vacas - QR Farm" 
-                                    class="img-fluid rounded-4 shadow-sm" 
-                                    style="max-height: 350px; width: 100%; object-fit: cover;">
+                                <img src="/src/assets/images/vacas-grupo.jpg" alt="Grupo de vacas - QR Farm" />
                             </div>
                             <div class="text-center px-2 px-sm-3 px-md-4">
                                 <p class="text-muted mb-2">Tu aliado inteligente en la gestión ganadera.</p>
