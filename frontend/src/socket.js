dc5a0e15d224c35aac690306b0e4e57b11e98deb
Tkex555 <bahamutthero@gmail.com>
2025-11-28 10:53:21 -0500

    arreglo para .env funcional local y docker

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+export const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:5000", {
   transports: ["websocket"],
   reconnectionAttempts: 5,
   reconnectionDelay: 3000,
